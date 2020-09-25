383bb0b420ba198c6ccefbf4a6315f8d2573089c
felkiriinya <felkiriinya@gmail.com>
2020-09-25 13:32:30 +0300

    proper set up of my working environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
+import { QuotesDetailsComponent } from './components/quotes-details/quotes-details.component';
+import { QuotesFormComponent } from './components/quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuotesDetailsComponent,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
