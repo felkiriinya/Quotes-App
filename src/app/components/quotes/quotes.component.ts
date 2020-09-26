e94c83451ba04f72715ed8b0653ceb4b1a5c5211
felkiriinya <felkiriinya@gmail.com>
2020-09-26 10:53:18 +0300

    Added the quotes array

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -1,4 +1,6 @@
+import { Quote } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from 'src/app/models/quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+    // Quotes Array
+    quotes:Quotes[]=[
+        new Quotes('Sometimes life is going to hit you in the head. Dont loose faith.','~Steve Jobs', new Date(2020,1,23)),
+        new Quotes('The greatest glory in living lies not in never falling, but in rising every time we fall.','~Nelson Mandela', new Date(2020,3,30)),
+        new Quotes('Competitions are for horses not artists','~Anonymous', new Date(2020,1,23)),
+        new Quotes('If life were predictable it would cease to be life, and be without flavor.','~Eleanor Roosevelt', new Date(2020,4,10)),
+        new Quotes('if you can make a woman laugh you can make her do almost anything','~Marlyn Monroe',new Date(2020,5,9)),
+        new Quotes('Life is what happens when you are making other plans','John Lennon', new Date(2020,6,12)),
+    ]
 
   constructor() { }
 
--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -1,2 +1,9 @@
 export class Quotes {
+    constructor(
+        public quote: string,
+        public author: string,
+        public submissionDate: Date,
+    ){
+
+    }
 }
