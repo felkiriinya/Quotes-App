e285fb989053d9bec5dbf534a1fa0d802aac9bda
felkiriinya <felkiriinya@gmail.com>
2020-09-27 20:42:05 +0300

    added the highlighting function

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -10,14 +10,14 @@ import { Quotes } from 'src/app/models/quotes';
 export class QuotesComponent implements OnInit {
     // Quotes Array
     quotes:Quotes[]=[
-        new Quotes('Sometimes life is going to hit you in the head. Dont loose faith.','Steve Jobs','Felista Kiriinya', new Date(2020,1,23),6,1),
+        new Quotes('Sometimes life is going to hit you in the head. Dont loose faith.','Steve Jobs','Felista Kiriinya', new Date(2020,1,23),2,1),
         new Quotes('The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela','Faith Mucami',new Date(2020,3,30),4,3),
         new Quotes('Competitions are for horses not artists','Anonymous', 'Eustine whels',new Date(2020,7,23),3,4),
         new Quotes('If life were predictable it would cease to be life, and be without flavor.','Eleanor Roosevelt', 'James Racher',new Date(2020,4,10),1,0),
-        new Quotes('If you can make a woman laugh you can make her do almost anything','Marlyn Monroe','Harry Thunder',new Date(2020,5,9),9,2),
+        new Quotes('The way to get started is to quit talking and begin doing','Walt Disney','Harry Thunder',new Date(2020,5,9),9,2),
         new Quotes('Life is what happens when you are making other plans','John Lennon', 'George Whither',new Date(2020,6,12),1,0),
     ]
-
+    
     //function for ading a new quote
     addNewQuote(quote) {
        
@@ -41,6 +41,52 @@ export class QuotesComponent implements OnInit {
             }
         }
     }
+
+    //function for getting the quote with the highest upvotes
+    // initialValue:number
+    // newValue:number
+    // counter:number
+    
+    // highlightHighest(){
+
+    //    this.initialValue=0
+    //    this.newValue=0
+
+    //    for (this.counter=0; this.counter<this.quotes.length; this.counter++){
+    //        this.newValue =this.quotes[this.counter].upVote;
+
+    //        if(this.newValue > this.initialValue){
+    //            this.initialValue =this.newValue
+    //        }
+    //        return this.initialValue
+    //    }
+
+
+    // }
+    
+    highlightHighest() {
+        
+        let quotesUpvote = []
+        let highestUpVote: number
+        for (let j = 0; j < this.quotes.length; j++) {
+          quotesUpvote.push(this.quotes[j].upVote)
+        }
+        
+            //The sort callback has to return
+
+            // a negative number if a < b
+            // 0 if a === b
+            // a positive number if a > b
+
+        quotesUpvote.sort(function (a, b) {
+            //if b>a then b-a>0 and will return a positive number
+          return b - a
+        })
+        highestUpVote = quotesUpvote[0]
+        return highestUpVote;
+      }
+    
+      
   constructor() { }
 
   ngOnInit(): void {
--- src/app/directives/highlight-quote.directive.ts
+++ src/app/directives/highlight-quote.directive.ts
@@ -1,12 +1,16 @@
-import { Directive, ElementRef } from '@angular/core';
-
-@Directive({
-  selector: '[appHighlightQuote]'
-})
-export class HighlightQuoteDirective {
-
-  constructor(private elem: ElementRef) { 
-      this.elem.nativeElement.style.background ="rgb(255, 0, 255)";
+import {
+    Directive,
+    ElementRef
+  } from '@angular/core';
+  
+  @Directive({
+    selector: '[appHighlightQuote]'
+  })
+  export class HighlightQuoteDirective {
+  
+    constructor(private elem: ElementRef) {
+      this.elem.nativeElement.style.background = "rgb(255, 0, 255)";
+    }
+  
   }
-
-}
+// rgb(255, 0, 255)
