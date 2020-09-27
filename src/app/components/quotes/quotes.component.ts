2186248fdec59ea74f6f5b439e05f36198bc4dc8
felkiriinya <felkiriinya@gmail.com>
2020-09-27 15:42:36 +0300

    added the highlight quote directive

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -12,7 +12,7 @@ export class QuotesComponent implements OnInit {
     quotes:Quotes[]=[
         new Quotes('Sometimes life is going to hit you in the head. Dont loose faith.','Steve Jobs','Felista Kiriinya', new Date(2020,1,23),6,1),
         new Quotes('The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela','Faith Mucami',new Date(2020,3,30),4,3),
-        new Quotes('Competitions are for horses not artists','Anonymous', 'Eustine whels',new Date(2020,1,23),3,4),
+        new Quotes('Competitions are for horses not artists','Anonymous', 'Eustine whels',new Date(2020,7,23),3,4),
         new Quotes('If life were predictable it would cease to be life, and be without flavor.','Eleanor Roosevelt', 'James Racher',new Date(2020,4,10),1,0),
         new Quotes('If you can make a woman laugh you can make her do almost anything','Marlyn Monroe','Harry Thunder',new Date(2020,5,9),9,2),
         new Quotes('Life is what happens when you are making other plans','John Lennon', 'George Whither',new Date(2020,6,12),1,0),
--- src/app/directives/highlight-quote.directive.ts
+++ src/app/directives/highlight-quote.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightQuote]'
 })
 export class HighlightQuoteDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) { 
+      this.elem.nativeElement.style.background ="rgb(255, 0, 255)";
+  }
 
 }
