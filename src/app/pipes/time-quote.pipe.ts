03892cb30bce3bc58193a0d3a3b7380fdfc07e95
felkiriinya <felkiriinya@gmail.com>
2020-09-27 14:55:32 +0300

    added the dates passed pipe

--- src/app/pipes/time-quote.pipe.ts
+++ src/app/pipes/time-quote.pipe.ts
@@ -5,8 +5,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeQuotePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    // getting current date
+
+    let currentDate = new Date();
+
+    //getting the difference in current time and time the quote was added
+    // getting the difference in miliseconds
+
+    let dateDifference = currentDate.getTime() - value.getTime();
+
+    //converting miliseconds into days
+
+    dateDifference=dateDifference /(24*60*60*1000);
+
+
+    //Rounding up a number to the next largest integer
+
+    return Math.ceil(dateDifference)-1;
   }
 
 }
